f2d465e3582815a9da22f5ff5a7e003b78c782c8
HYEJIN KWON <chahaa0189@gmail.com>
2026-02-15 19:42:08 +0900

    feat : main.js에 랜덤 번호 기능 구현 및 button 클릭시 랜덤값과 userValue값 비교하여 결과창 나타내기 구현

---
+++ main.js
@@ -0,0 +1,34 @@
+//랜덤 번호 지정
+//유저가 번호를 입력한다 go라는 버튼 누름
+//랜덤 번호가 < 유저번호 down
+//랜덤 번호가 > 유저번호  up
+//reset버튼을 누르면 게임 리셋
+//5번의 기회를 다 쓰면 게임이 끝난다 (더이상 추측 불가, 버튼이 disable)
+//유저가 1~100 범위 밖에 숫자를 입력하면 알려준다. 기회를 깎지 않는다.
+//유저가 이미 입력한 숫자를 또 입력하면 알려준다 기회를 깎지 않는다.
+
+let computerNum =0
+let playButton = document.getElementById("play-button")
+let userInput = document.getElementById("user-input")
+let resultArea = document.getElementById("result-area")
+
+playButton.addEventListener("click",play)
+
+const pickRandomNum = ()=>{
+      computerNum = Math.floor(Math.random()*100) + 1
+    console.log(computerNum)
+}
+pickRandomNum()
+
+function play(){
+
+    let userValue = userInput.value
+    console.log(userValue)
+    if(userValue<computerNum){
+        resultArea.textContent = "UP!!"
+    }else if(userValue>computerNum){
+       resultArea.textContent = "Down!!"
+    }else {
+       resultArea.textContent = "맞췄습니다!!"
+    }
+}
