daf9b2b6b9af1553adcb413f9b1fa454227137b3
HYEJIN KWON <chahaa0189@gmail.com>
2026-02-16 17:37:05 +0900

    feat : reset 버튼 구현 , 유효성 검사 구현

--- main.js
+++ main.js
@@ -11,8 +11,16 @@ let computerNum =0
 let playButton = document.getElementById("play-button")
 let userInput = document.getElementById("user-input")
 let resultArea = document.getElementById("result-area")
-
+let resetButton = document.getElementById('reset-button')
+let chance =5
+let gameOver = false
+let chanceArea = document.getElementById('chance-area')
+let history = []
+userInput.addEventListener("focus",()=>{
+    userInput.value = ""
+})
 playButton.addEventListener("click",play)
+resetButton.addEventListener("click",reset)
 
 const pickRandomNum = ()=>{
       computerNum = Math.floor(Math.random()*100) + 1
@@ -22,7 +30,18 @@ pickRandomNum()
 
 function play(){
 
+  
     let userValue = userInput.value
+    if(userValue<1 || userValue>100){
+        resultArea.textContent = "1과 100사이 숫자를 입력해주세요"
+        return
+    }
+    if(history.includes(userValue)){
+         resultArea.textContent = "이미 입력된 값입니다."
+        return
+    }
+    chance--
+    chanceArea.textContent = `남은기회 : ${chance}`
     console.log(userValue)
     if(userValue<computerNum){
         resultArea.textContent = "UP!!"
@@ -30,5 +49,31 @@ function play(){
        resultArea.textContent = "Down!!"
     }else {
        resultArea.textContent = "맞췄습니다!!"
+       gameOver = true
     }
+
+    history.push(userValue)
+
+    if(chance<1){
+        gameOver = true
+    }
+    if(gameOver){
+        playButton.disabled = true;
+    }
+   
+
+
+}
+
+function reset(){
+    //userinput 창 깨긋하게 정리
+    // 새로운 번호 생성
+    userInput.value = ""
+    chance = 5
+    chanceArea.textContent = `남은기회 : ${chance}`
+    pickRandomNum()
+    gameOver = false
+    playButton.disabled = false
+    resultArea.textContent = "결과값이 여기 나옵니다"
+
 }
